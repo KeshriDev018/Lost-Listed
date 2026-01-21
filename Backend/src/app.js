98c3ce2859cd6d0317245c77f82ba7d926ab411f
Ankit Keshri <ankitkeshari@gmail.com>
2026-01-21 16:43:30 +0530

    made changes regarding Frontend url in the env  and app.js before deployingBackend to render

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,12 +11,23 @@ const app = express()
 
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
+
+
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // must exactly match your frontend URL
-    credentials: true, // allow sending cookies
-  })
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
 );
+
 
 
 
